a5e6c2025e8b5a0955aca1dc83b2f573aa36679f
burak <burakoncuy@gmail.com>
2024-12-17 23:05:52 -0500

    updated

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -30,41 +30,41 @@ router.use('/review-images', reviewImagesRouter);
 
 
 /********************** Test Routes **********************/
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
 
-// // GET /api/set-token-cookie
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
-// GET /api/restore-user
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+GET /api/restore-user
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
-// GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 /********************** END of Test Routes **********************/
 
 
